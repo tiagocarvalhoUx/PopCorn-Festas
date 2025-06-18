b5e3faf21831eed0ac580aad302b099f9f564d5a
nome <email>
2025-06-18 04:48:05 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/meu-app/', // Se você tem um subdiretório no deploy
-})
+});
